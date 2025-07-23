c28b9271b57a577c82fb3facacbb86cdf0454208
conialejandra <constanza.luengo@mail.udp.cl>
2025-07-23 18:35:48 -0400

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* ----- index.html ----- */
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,7 +15,6 @@
 </html>
 
 
-/* ----- styles.css ----- */
 body {
   font-family: sans-serif;
   background-color: #e6f7ff;
